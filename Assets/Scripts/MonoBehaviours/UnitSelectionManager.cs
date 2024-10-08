bf02275804681f5331ccf890aa77d22e4ed17c24
Seona <seona13@gmail.com>
2024-10-09 10:51:49 +1100

    Unit Selection Multiple, UI Setup
    - Use mouse screen position to define a rectangle selection area.
    - Tie a visual to the selection rectangle to make it easy to see what's being encompassed.
    - Check which units are inside the selected rectangle.
    - Enable the Selected status of those units.

--- Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
+++ Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
@@ -1,12 +1,67 @@
+using System;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Transforms;
 using UnityEngine;
 
 public class UnitSelectionManager : MonoBehaviour
 {
+    public static UnitSelectionManager Instance { get; private set; }
+
+    public event EventHandler OnSelectionAreaStart;
+    public event EventHandler OnSelectionAreaEnd;
+
+    private Vector2 selectionStartMousePosition;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0)) // Start unit selection
+        {
+            selectionStartMousePosition = Input.mousePosition;
+            OnSelectionAreaStart?.Invoke(this, EventArgs.Empty);
+        }
+
+        if (Input.GetMouseButtonUp(0)) // End unit selection
+        {
+            Vector2 selectionEndMousePosition = Input.mousePosition;
+
+            EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            EntityQuery entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<Selected>().Build(entityManager);
+
+            NativeArray<Entity> entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+            for (int i = 0; i < entityArray.Length; i++)
+            {
+                entityManager.SetComponentEnabled<Selected>(entityArray[i], false);
+            }
+            
+            entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<LocalTransform, Unit>().WithPresent<Selected>().Build(entityManager);
+
+            Rect selectionAreaRect = GetSelectionAreaRect();
+
+            entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+            NativeArray<LocalTransform> localTransformArray = entityQuery.ToComponentDataArray<LocalTransform>(Allocator.Temp);
+            for (int i = 0; i < localTransformArray.Length; i++)
+            {
+                LocalTransform unitLocalTransform = localTransformArray[i];
+                Vector2 unitScreenPosition = Camera.main.WorldToScreenPoint(unitLocalTransform.Position);
+                if (selectionAreaRect.Contains(unitScreenPosition))
+                {
+                    // Unit is inside the selection area
+                    entityManager.SetComponentEnabled<Selected>(entityArray[i], true);
+                }
+            }
+
+            OnSelectionAreaEnd?.Invoke(this, EventArgs.Empty);
+        }
+
+        if (Input.GetMouseButtonDown(1)) // Issue move command
         {
             Vector3 mouseWorldPosition = MouseWorldPosition.Instance.GetPosition();
 
@@ -23,5 +78,27 @@ public class UnitSelectionManager : MonoBehaviour
             }
             entityQuery.CopyFromComponentDataArray(unitMoverArray);
         }
+    }
+
+
+    public Rect GetSelectionAreaRect()
+    {
+        Vector2 selectionEndMousePosition = Input.mousePosition;
+
+        Vector2 lowerLeftCorner = new Vector2(
+            Mathf.Min(selectionStartMousePosition.x, selectionEndMousePosition.x),
+            Mathf.Min(selectionStartMousePosition.y, selectionEndMousePosition.y)
+            );
+        Vector2 upperRightCorner = new Vector2(
+            Mathf.Max(selectionStartMousePosition.x, selectionEndMousePosition.x),
+            Mathf.Max(selectionStartMousePosition.y, selectionEndMousePosition.y)
+            );
+
+        return new Rect(
+            lowerLeftCorner.x,
+            lowerLeftCorner.y,
+            upperRightCorner.x - lowerLeftCorner.x,
+            upperRightCorner.y - lowerLeftCorner.y
+            );
     }
 }
