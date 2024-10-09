c4f808f6b380225c68ee564096b3571e89221b28
Seona <seona13@gmail.com>
2024-10-09 13:08:16 +1100

    Unit Events
    - Use booleans on Selected component to simulate event triggers in the absence of delegates.
    - Set onSelected and onDeselected to true at appropriate times.
    - Reset all "event" booleans to false at the end of the frame.
    - Updated the Selected Visual System to only run when the appropriate boolean is true.

--- Assets/Scripts/Authoring/SelectedAuthoring.cs
+++ Assets/Scripts/Authoring/SelectedAuthoring.cs
@@ -5,6 +5,9 @@ public struct Selected : IComponentData, IEnableableComponent
 {
     public Entity visualEntity;
     public float showScale;
+
+    public bool onSelected;
+    public bool onDeselected;
 }
 
 
--- Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
+++ Assets/Scripts/MonoBehaviours/UnitSelectionManager.cs
@@ -1,6 +1,7 @@
 using System;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Entities.UniversalDelegates;
 using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Transforms;
@@ -40,9 +41,13 @@ public class UnitSelectionManager : MonoBehaviour
             // Deselected all currently-selected units
             EntityQuery entityQuery = new EntityQueryBuilder(Allocator.Temp).WithAll<Selected>().Build(entityManager);
             NativeArray<Entity> entityArray = entityQuery.ToEntityArray(Allocator.Temp);
+            NativeArray<Selected> selectedArray = entityQuery.ToComponentDataArray<Selected>(Allocator.Temp);
             for (int i = 0; i < entityArray.Length; i++)
             {
                 entityManager.SetComponentEnabled<Selected>(entityArray[i], false);
+                Selected selected = selectedArray[i];
+                selected.onDeselected = true;
+                entityManager.SetComponentData(entityArray[i], selected);
             }
 
             // Test to see if multiple or single selection
@@ -65,6 +70,9 @@ public class UnitSelectionManager : MonoBehaviour
                     {
                         // Unit is inside the selection area
                         entityManager.SetComponentEnabled<Selected>(entityArray[i], true);
+                        Selected selected = entityManager.GetComponentData<Selected>(entityArray[i]);
+                        selected.onSelected = true;
+                        entityManager.SetComponentData(entityArray[i], selected);
                     }
                 }
             } else
@@ -92,6 +100,9 @@ public class UnitSelectionManager : MonoBehaviour
                     {
                         // Hit a unit
                         entityManager.SetComponentEnabled<Selected>(raycastHit.Entity, true);
+                        Selected selected = entityManager.GetComponentData<Selected>(raycastHit.Entity);
+                        selected.onSelected = true;
+                        entityManager.SetComponentData(raycastHit.Entity, selected);
                     }
                 }
             }
